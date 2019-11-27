9e781ce80e6b11bff7e435bb636b5279ccd6488d
waterhog <geoff.s.waterhouse@gmail.com>
2019-11-27 08:57:41 -0800

    install bash added

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Cloning project from github..."
+
+git clone https://github.com/waterhog/gaia_plastics /home/pi/gaia_plastics
+
+echo "Installing dependencies (this may take some time)..."
+
+sudo apt-get update
+
+sudo apt-get install libhdf5-dev -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install libjasper-dev  -y
+sudo apt-get install libqtgui4  -y
+sudo apt-get install libqt4-test -y
+sudo apt-get install tdsodbc -y
+sudo apt-get install unixodbc-dev -y
+
+pip3 install opencv-python==3.4.4.19
+pip3 install opencv-contrib-python==3.4.4.19
+pip3 install pyodbc
+pip3 install pyserial
+
+echo "Copying odbcinst.ini to /etc/..."
+
+sudo cp /home/pi/gaia_plastics/app/conf/odbc/odbcinst.ini /etc/
+
+echo "Setting up WiFi IP address emailing and application to run when RPI powered on..."
+
+sudo sed -i '/^exit 0.*/i sudo python /home/pi/gaia_plastics/email_ip.py\npython3 /home/pi/gaia_plastics/app/code/main.py' /etc/rc.local
+
+echo "Done with software setup"
